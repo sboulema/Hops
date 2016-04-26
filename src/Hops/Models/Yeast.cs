85bf294ac975c631909a47602502a5ecd027affd
Samir L. Boulema <sboulema@gmail.com>
2016-04-26 10:30:37 +0200

    Added Attenuation to yeast

--- src/Hops/Models/Yeast.cs
+++ src/Hops/Models/Yeast.cs
@@ -8,10 +8,17 @@
         public int Lab { get; set; }
         public int TempMin { get; set; }
         public int TempMax { get; set; }
+        public int AttenuationMin { get; set; }
+        public int AttenuationMax { get; set; }
 
         public string Temp()
         {
-            return FormatValues(TempMin, TempMax);
+            return $"{FormatValues(TempMin, TempMax)} &deg;C";
+        }
+
+        public string Attenuation()
+        {
+            return $"{FormatValues(AttenuationMin, AttenuationMax)}%";
         }
 
         private string FormatValues(double min, double max)
